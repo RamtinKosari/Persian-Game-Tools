bac80246d9393a354234351f8d675d485af16630
RamtinKosari <ramtinkosari@gmail.com>
2023-08-23 20:31:22 +0330

    Finish Fandogh Solver Alpha

--- Guess Words/Fandogh/main.cpp
+++ Guess Words/Fandogh/main.cpp
@@ -8,6 +8,7 @@
 # define SUCCESS "\033[38;2;0;255;0m[SUCCESS]\033[0m "
 # define ANSWER "\033[38;2;0;255;0m[ANSWER]\033[0m "
 # define FAILED "\033[38;2;255;0;0m[FAILED]\033[0m "
+# define EMPTY "\033[38;2;255;200;0m[EMPTY]\033[0m "
 # define LOG "\033[38;2;150;150;150m[LOG]\033[0m "
 # define IP "\033[38;2;150;150;0m[IP]\033[0m"
 # define TAB "   "
@@ -49,24 +50,25 @@ int main(int argc, char* argv[]) {
     //-- Calculates Maximum Size of Word that is Possible According to Data File
     int max_size = 0;
     for (int i = 0; i < words.size(); i++) {
-
+        if (words[i].size() > max_size) {
+            max_size = words[i].size();
+        }
     }
     //-- Client Section
-    int amount = 0;
     int point_value = 0;
     int accept_value = 0;
-    int accept_amount = 0;
     std::wstring search_key;
     int accept_amount_with_letters = 0;
-    std::vector<std::wstring> accepted;
-    std::vector<std::wstring> suggestions;
     std::vector<std::wstring> accepted_with_letters;
     std::wcout << LOG "Enter Letters Concatenated to each Other : "; std::wcin >> search_key;
+    if (search_key.size() <= 1) {
+        std::wcout << FAILED "At Least Two Letters Should be Passed" ENDL;
+        exit(EXIT_FAILURE);
+    }
     std::wcout << TAB LOG "Letters : " ENDL;
     for (wchar_t letter : search_key) {
         std::wcout << TAB TAB LOG << letter << ENDL;
     }
-    std::wcout << LOG "Amount of All Suggestions : " << amount << ENDL;
     std::wcout << LOG "Generating Possibilities ..." ENDL;
     for (int i = 0; i < words.size(); i++) {
         accept_value = words[i].size();
@@ -84,10 +86,21 @@ int main(int argc, char* argv[]) {
         point_value = 0;
     }
     if (accept_amount_with_letters > 0) {
-        std::wcout << TAB SUCCESS << accept_amount_with_letters << " Words have been Found Totally as Follows : " ENDL;
-        // for (int i = 0; i < accepted_with_letters.size(); i++) {
-        //     std::wcout << TAB TAB ANSWER << accepted_with_letters[i] << ENDL;
-        // }
+        unsigned int amount = 0;
+        for (int i = 2; i < max_size; i++) {
+            std::wcout << TAB SUGGEST "Suggestions Words with " << i << " Letters :" ENDL;
+            for (int j = 0; j < accepted_with_letters.size(); j++) {
+                if (accepted_with_letters[j].size() == i) {
+                    std::wcout << TAB TAB ANSWER << accepted_with_letters[j] << ENDL;
+                    amount++;
+                }
+            }
+            if (amount == 0) {
+                std::wcout << TAB TAB EMPTY "Can Not Find any According to Data File" ENDL;
+            }
+            amount = 0;
+        }
+        std::wcout << TAB SUCCESS "Total Amount of Words with Combination of Your Letters :" ENDL;
     } else {
         std::wcout << TAB FAILED "No Accepted Word Found According to Data File" ENDL;
     }
